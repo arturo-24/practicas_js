3eef792718b077fcf2df34a4d4ee9301819db989
arturo-24 <arturo_cochea@hotmail.com>
2025-02-11 22:36:48 -0500

    Segundo ejercicio

--- js/01.js
+++ js/01.js
@@ -1,7 +1,35 @@
-/* variables */
+/* multiples variables */
 
 var nombre = "Arthur",
     disponible1 = true,
     categoria = "Programador"; /* Este es un sintaxis de agregar 3 variables en un solo var*/
 
-var nombre;
+
+
+// Variables
+
+var nombre = "Arthur"; // iniciamos la variable y su respectivo valor
+
+var disponible; // inciiamos la variable pero sin valor
+
+producto = true; // Reasignando el valor de la variable
+
+
+// LO QUE NO PUEDES HACER en JavaScript : 
+ 
+// var 1disponible; // NO PUEDES INCIAR UNA VARIABLE CON UN NOMBRE NUMERICO (TE SALDA ERROR EN AL CONSOLE)
+
+// var -dispobible; NO CON GUION BAJO PODRAS INCIAR
+
+var _disponible; // SI PODES INCIAR UNA VARIABLE CON UN NOMBRE CON GUION BAJO O COLOCALRO AL FINAL.
+
+
+// ESTILOS DE VARIABLES
+
+var nombre_producto = 'montior hd'; // underscore
+var nombreProducto = 'montior hd'; // CamelCase
+var NombreProducto = 'montior hd'; // PascalCase
+var nobmreproducto = 'montior hd'; // 
+
+
+console.log(nombre_producto);
---
+++ js/02.js
@@ -0,0 +1,37 @@
+// letaibles con let
+
+
+
+var nombre = "Arthur",
+    disponible1 = true,
+    categoria = "Programador"; /* Este es un sintaxis de agregar 3 letiables en un solo let*/
+
+
+
+// letiables
+
+let nombre = "Arthur"; // iniciamos la letiable y su respectivo valor
+
+let disponible; // inciiamos la letiable pero sin valor
+
+producto = true; // Reasignando el valor de la letiable
+
+
+// LO QUE NO PUEDES HACER en JavaScript : 
+ 
+// let 1disponible; // NO PUEDES INCIAR UNA letIABLE CON UN NOMBRE NUMERICO (TE SALDA ERROR EN AL CONSOLE)
+
+// let -dispobible; NO CON GUION BAJO PODRAS INCIAR
+
+let _disponible; // SI PODES INCIAR UNA letIABLE CON UN NOMBRE CON GUION BAJO O COLOCALRO AL FINAL.
+
+
+// ESTILOS DE letIABLES
+
+let nombre_producto = 'montior hd'; // underscore
+let nombreProducto = 'montior hd'; // CamelCase
+let NombreProducto = 'montior hd'; // PascalCase
+let nobmreproducto = 'montior hd'; // 
+
+
+console.log(nombre_producto);
