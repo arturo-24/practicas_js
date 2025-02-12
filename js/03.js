a636351dfb4196bd6a7f30909bc6ee3f79b0f242
arturo-24 <arturo_cochea@hotmail.com>
2025-02-12 15:32:44 -0500

    3cer ejercicio

---
+++ js/03.js
@@ -0,0 +1,29 @@
+
+
+// constiables
+
+ // iniciamos la constiable y su respectivo valor
+
+const disponible = true; // 
+
+producto = true; // Reasignando el valor de la constiable
+
+// const dispobible; // NO PUEDES COLOCAR UNA VARAIBLE PERO EL VALOR DE DICHA VARAIBLE NO LO CONOCES ENTONCES NO PUEDES USAR EL VALOR DE LA CONST".
+
+
+ 
+// const 1disponible; // NO PUEDES INCIAR UNA constIABLE CON UN NOMBRE NUMERICO (TE SALDA ERROR EN AL CONSOLE)
+
+// const -dispobible; NO CON GUION BAJO PODRAS INCIAR
+
+const _disponible = true; // 
+
+// ESTILOS DE constIABLES
+
+const nombre_producto = 'montior hd'; // underscore
+const nombreProducto = 'montior hd'; // CamelCase
+const NombreProducto = 'montior hd'; // PascalCase
+const nobmreproducto = 'montior hd'; // 
+
+
+console.log(nombre_producto);
