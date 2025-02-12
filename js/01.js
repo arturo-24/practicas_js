fdc5efe2eee58f9c741249efa18b18a2bcf9f30e
arturo-24 <arturo_cochea@hotmail.com>
2025-02-11 19:06:31 -0500

    second commit

---
+++ js/01.js
@@ -0,0 +1,7 @@
+/* variables */
+
+var nombre = "Arthur",
+    disponible1 = true,
+    categoria = "Programador"; /* Este es un sintaxis de agregar 3 variables en un solo var*/
+
+var nombre;
---
+++ js/app.js
@@ -0,0 +1,2 @@
+alert("Alerta estas siendo hackeado ahora todo tu informacion sera de los unicornios!")
+
