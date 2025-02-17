8be06ee7b2c70e91316e252524781e57cdbb177a
arturo-24 <arturo_cochea@hotmail.com>
2025-02-17 14:37:42 -0500

    Creacion de 6.js

---
+++ js/06.js
@@ -0,0 +1,18 @@
+//  Objetos Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.5);
+resultado = Math.round(3.5); //redondear por "round"
+resultado = Math.ceil(5.3); // CEil siempre redondea hacia arriba
+resultado = Math.floor(5.3); // Floor siempre redondea hacia abajo (lo contrario de ceil)
+resultado = Math.sqrt(144); //Raiz cuadrada de 144.
+resultado = Math.abs(-144); // cumbirte en valores negativos a positivos.
+resultados = Math.min ( 3, 5, 2, 1, 4); // Min es el menor de un listaado.
+resultados = Math.max ( 3, 5, 2, 1, 4); // EL maximo de un listaado.
+resultados = Math.random (); // EL maximo de un listaado.
+resultados = Math.floor(Math.random() * 30); // EL maximo de un listaado.
+
+
+console.log(resultado); 
